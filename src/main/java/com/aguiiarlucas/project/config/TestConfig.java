f688677638190be1a58291183677dd71bffa2176
aguiiarlucas <lucascostaaguiar.com>
2021-07-10 14:27:09 -0300

    OrderStatus Enum

--- src/main/java/com/aguiiarlucas/project/config/TestConfig.java
+++ src/main/java/com/aguiiarlucas/project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.aguiiarlucas.project.entities.Order;
 import com.aguiiarlucas.project.entities.User;
+import com.aguiiarlucas.project.entities.enums.OrderStatus;
 import com.aguiiarlucas.project.repositories.OrderRepository;
 import com.aguiiarlucas.project.repositories.UserRepository;
 
@@ -31,11 +32,11 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@hotmail.com", "997205369", "123456");
 		User u2 = new User(null, "Bob Green", "bob@hotmail.com", "32037565", "789456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID,u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED,u1);
 
-		userRepository .saveAll(Arrays.asList(u1,u2)); // [ Cria uma lista e salva no Banco de Dados ] 
+		userRepository .saveAll(Arrays.asList(u1,u2)); //[ Cria uma lista e salva no Banco de Dados]
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
