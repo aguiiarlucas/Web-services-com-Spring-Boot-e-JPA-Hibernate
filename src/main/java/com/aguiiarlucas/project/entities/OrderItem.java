a5e805c7759eaf2273bd6988747e7f27e5e05210
aguiiarlucas <lucascostaaguiar.com>
2021-07-17 15:16:06 -0300

    SubTotal and Total

--- src/main/java/com/aguiiarlucas/project/entities/OrderItem.java
+++ src/main/java/com/aguiiarlucas/project/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price*quantity;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,4 +94,5 @@ public class OrderItem implements Serializable {
 		return true;
 	}
 
+	
 }
