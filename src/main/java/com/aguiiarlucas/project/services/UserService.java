4857c18201694a07523300b7cf77a952a3d89cb9
aguiiarlucas <lucascostaaguiar.com>
2021-07-19 23:51:24 -0300

    Exception Udpate and Delete

--- src/main/java/com/aguiiarlucas/project/services/UserService.java
+++ src/main/java/com/aguiiarlucas/project/services/UserService.java
@@ -3,11 +3,17 @@ package com.aguiiarlucas.project.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aguiiarlucas.project.entities.User;
 import com.aguiiarlucas.project.repositories.UserRepository;
+import com.aguiiarlucas.project.services.exceptions.DatabaseException;
+import com.aguiiarlucas.project.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,20 +28,38 @@ public class UserService {
 
 	 public User findById( Long id ) {
 		 Optional<User> obj = repository.findById(id);
-		 return obj.get();
+		 return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	 }
 	 public User insert (User obj ) { //[Insere um novo obj User]
 		 return repository.save(obj);
 	 }
 	 
 	 public void delete ( Long id ) {
+		 try {
 		 repository.deleteById(id);
+	 }catch(EmptyResultDataAccessException e ) {
+		 e.printStackTrace();
+		 throw new ResourceNotFoundException(id);
+	 }catch(DataIntegrityViolationException e) {
+		 throw new DatabaseException(e.getMessage());
 	 }
+		 
+	 }
+
+	 
 	 public User updateData ( Long id , User obj) {
-		 User entity = repository.getOne(id);
-		 updateData(entity, obj);
-		 return repository.save(entity);
+		 
+		 try{
+			 User entity = repository.getOne(id);
+			 updateData(entity, obj);
+			 return repository.save(entity);
+		 }catch (EntityNotFoundException e) {
+			 e.printStackTrace();
+			 throw new ResourceNotFoundException(id);
+		}
 	 }
+		
+	 
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
---
+++ src/main/java/com/aguiiarlucas/project/services/exceptions/DatabaseException.java
@@ -0,0 +1,14 @@
+package com.aguiiarlucas.project.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+ 
+}
